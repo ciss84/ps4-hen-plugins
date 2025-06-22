18f5cc369fcda6a6d319858790e9d6ba1f0fb86c
Mugi <37186888+ciss84@users.noreply.github.com>
2025-06-22 14:40:06 +0200

    Update plugin_common.h

--- common/plugin_common.h
+++ common/plugin_common.h
@@ -1,4 +1,19 @@
 #include "git_ver.h"
+#include <Common.h>
+
+#define GOLDHEN_PATH "/data/lso"
+
+#define u8  uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+#define u64 uint64_t
+#define s8  int8_t
+#define s16 int16_t
+#define s32 int32_t
+#define s64 int64_t
+#define f32 float
+#define f64 double
+#define TEX_ICON_SYSTEM "https://ciss84.github.io/GtaVModOnlinePng/icon1.png" //"cxml://psnotification/tex_default_icon_cloud_client" //"cxml://psnotification/tex_icon_system"
 
 #define MAX_PATH_ 260
 
@@ -14,6 +29,26 @@
 #endif
 
 #define final_printf(a, args...) printf("(%s:%d) " a, __FILE__, __LINE__, ##args)
+#define boot_ver() {\
+    final_printf("Commit: %s Branch: %s Build: %i %s\n", GIT_COMMIT, GIT_VER, GIT_NUM, BUILD_TYPE);\
+    final_printf("Built: %s\n", BUILD_DATE);\
+    final_printf("GoldHEN SDK Ver: 0x%08x\n", GOLDHEN_SDK_VERSION);\
+    final_printf("GoldHEN System SDK Ver: 0x%08x\n", sys_sdk_version());\
+}
+
+#define print_proc_info() {\
+    final_printf("process info\n");\
+    final_printf("pid: %d\n", procInfo.pid);\
+    final_printf("name: %s\n", procInfo.name);\
+    final_printf("path: %s\n", procInfo.path);\
+    final_printf("titleid: %s\n", procInfo.titleid);\
+    final_printf("contentid: %s\n", procInfo.contentid);\
+    final_printf("version: %s\n", procInfo.version);\
+    final_printf("base_address: 0x%lx\n", procInfo.base_address);\
+}
+
+void NotifyStatic(const char* IconUri, const char *text);
+void Notify(const char* IconUri, const char *FMT, ...);
 
 // Takes hardcoded input string 2 to strlen against during compile time.
 // startsWith(input_1, "input 2");
