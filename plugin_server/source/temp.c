05331bcc46431daf2d074df7e7453afe39e59faa
Mugi <37186888+ciss84@users.noreply.github.com>
2025-06-24 18:05:28 +0200

    Update temp.c

--- plugin_server/source/temp.c
+++ plugin_server/source/temp.c
@@ -1,11 +1,11 @@
 void* pthread_temp(void* args)
 {
     (void)args;
-    uint32_t (*sceKernelGetCpuTemperature)(uint32_t *);
-    uint32_t (*sceKernelGetSocSensorTemperature)(uint32_t, uint32_t *);
+    int (*sceKernelGetCpuTemperature)(int *);
+    int (*sceKernelGetSocSensorTemperature)(int, int *);
     while (1)
     {
-        uint32_t CPU_Temp, SOC_Temp;
+        int CPU_Temp, SOC_Temp;
         sceKernelGetCpuTemperature(&CPU_Temp);
         sceKernelGetSocSensorTemperature(0, &SOC_Temp); 
         Notify(TEX_ICON_SYSTEM,"*Cpu: %d*C\n*Soc: %i*C", CPU_Temp, SOC_Temp);}
