ee2df07407a012ac78a5a8bd371d59a46bc522a8
Mugi <37186888+ciss84@users.noreply.github.com>
2025-06-24 15:40:38 +0200

    Update lib.cpp

--- plugin_temp/source/lib.cpp
+++ plugin_temp/source/lib.cpp
@@ -7,7 +7,7 @@
 #include "../../common/plugin_common.h"
 #include "../../common/notify.h"
 
-attr_public const char* g_pluginName = "plugin_example";
+attr_public const char* g_pluginName = "plugin_temp";
 attr_public const char* g_pluginDesc = "Demonstrate usage of CXX in module. Based from OpenOrbis `library_example`";
 attr_public const char* g_pluginAuth = "illusiony";
 attr_public uint32_t g_pluginVersion = 0x00000100;  // 1.00
@@ -17,17 +17,11 @@ uint32_t (*sceKernelGetSocSensorTemperature)(uint32_t, uint32_t *);
 
 extern "C" int plugin_load(int* argc, const char** argv)
 {
-    while (1)
-        {
-            uint32_t CPU_Temp, SOC_Temp;
-            sceKernelGetCpuTemperature(&CPU_Temp);
-            sceKernelGetSocSensorTemperature(0, &SOC_Temp);
-            std::ostringstream os;
-            os << "*Cpu: %d*C\n*Soc: %i*C" << CPU_Temp << SOC_Temp
-               << "You passed: " << *argc << " args\n"
-               << "Built: " << __TIME__ << " " << __DATE__;
-            Notify(TEX_ICON_SYSTEM, os.str().c_str());
-            sceKernelSleep(100);
-        }
-    return 0;
+//while (1){
+     uint32_t CPU_Temp, SOC_Temp;
+     sceKernelGetCpuTemperature(&CPU_Temp);
+     sceKernelGetSocSensorTemperature(0, &SOC_Temp);
+     Notify(TEX_ICON_SYSTEM, "*Cpu: %d*C\n*Soc: %i*C", CPU_Temp, SOC_Temp);
+     //sceKernelSleep(100);
+   //}
 }
