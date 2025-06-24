40e1f04baa5437b4cdde59a4e23d506d9be0eaa9
Mugi <37186888+ciss84@users.noreply.github.com>
2025-06-24 16:33:52 +0200

    Update lib.cpp

--- plugin_temp/source/lib.cpp
+++ plugin_temp/source/lib.cpp
@@ -12,16 +12,21 @@ attr_public const char* g_pluginDesc = "Demonstrate usage of CXX in module. Base
 attr_public const char* g_pluginAuth = "illusiony";
 attr_public uint32_t g_pluginVersion = 0x00000100;  // 1.00
 
+int temp;
 uint32_t (*sceKernelGetCpuTemperature)(uint32_t *);
 uint32_t (*sceKernelGetSocSensorTemperature)(uint32_t, uint32_t *);
 
 extern "C" int plugin_load(int* argc, const char** argv)
 {
-//while (1){
-     uint32_t CPU_Temp, SOC_Temp;
-     sceKernelGetCpuTemperature(&CPU_Temp);
-     sceKernelGetSocSensorTemperature(0, &SOC_Temp);
-     Notify(TEX_ICON_SYSTEM, "*Cpu: %d*C\n*Soc: %i*C", CPU_Temp, SOC_Temp);
-     //sceKernelSleep(100);
-   //}
+  temp= 1;
+  while (temp) {
+  uint32_t CPU_Temp;
+  uint32_t SOC_Temp;
+  uint32_t ret = sceKernelGetCpuTemperature(&CPU_Temp);
+  uint32_t retb = sceKernelGetSocSensorTemperature(0, &SOC_Temp);
+  Sleep(100);
+  printf("returned %d\n%i", ret, retb);
+  Notify(TEX_ICON_SYSTEM, "Temperature\n*Cpu: %d*C\n*Soc: %i*C", CPU_Temp, SOC_Temp);
+  }
+  return 0;      
 }
