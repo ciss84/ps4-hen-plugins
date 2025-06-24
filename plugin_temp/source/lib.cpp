94643268c4b05be3667117dce411259470dfdfb8
Mugi <37186888+ciss84@users.noreply.github.com>
2025-06-24 17:30:38 +0200

    test

--- plugin_temp/source/lib.cpp
+++ plugin_temp/source/lib.cpp
@@ -7,25 +7,18 @@
 #include "../../common/plugin_common.h"
 #include "../../common/notify.h"
 
-attr_public const char* g_pluginName = "plugin_temp";
+attr_public const char* g_pluginName = "plugin_example";
 attr_public const char* g_pluginDesc = "Demonstrate usage of CXX in module. Based from OpenOrbis `library_example`";
 attr_public const char* g_pluginAuth = "illusiony";
 attr_public uint32_t g_pluginVersion = 0x00000100;  // 1.00
 
-static uint32_t (*sceKernelGetCpuTemperature)(uint32_t *);
-static uint32_t (*sceKernelGetSocSensorTemperature)(uint32_t, uint32_t *);
-
-int plugin_load(int* argc, const char** argv)
+extern "C" int plugin_load(int* argc, const char** argv)
 {
-  int temp = 1;
-  while (temp) {
-  uint32_t CPU_Temp;
-  uint32_t SOC_Temp;
-  uint32_t ret = sceKernelGetCpuTemperature(&CPU_Temp);
-  uint32_t retb = sceKernelGetSocSensorTemperature(0, &SOC_Temp);
-  sceKernelSleep(100);
-  printf("returned %d\n%i", ret, retb);
-  Notify(TEX_ICON_SYSTEM, "Temperature\n*Cpu: %d*C\n*Soc: %i*C", CPU_Temp, SOC_Temp);
-  }
-  return 0;      
+    // https://github.com/OpenOrbis/OpenOrbis-PS4-Toolchain/blob/63c0be5ffff09fbaebebc6b9a738d150e2da0205/samples/library_example/library_example/lib.cpp
+    std::ostringstream os;
+    os << "Hi I'm from the library!\n"
+       << "You passed: " << *argc << " args\n"
+       << "Built: " << __TIME__ << " " << __DATE__;
+    Notify(TEX_ICON_SYSTEM, os.str().c_str());
+    return 0;
 }
