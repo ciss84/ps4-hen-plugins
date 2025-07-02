e55d5ae492ce45e0a76fc7738e802dc6b6341a48
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-02 12:23:52 +0200

    Update lib.cpp

--- plugin_loader/source/lib.cpp
+++ plugin_loader/source/lib.cpp
@@ -14,9 +14,7 @@ extern "C" int plugin_load(SceEntry* args, const void* atexit_handler)
     // https://github.com/OpenOrbis/OpenOrbis-PS4-Toolchain/blob/63c0be5ffff09fbaebebc6b9a738d150e2da0205/samples/library_example/library_example/lib.cpp
     // Just a copy of example for nowAdd commentMore actions
     std::ostringstream os;
-    os << "Hi I'm from the library!\n"
-       << "You passed: " << *args << " args\n"
-       << "Built: " << __TIME__ << " " << __DATE__;
+    os << "Hi I'm from the library!\n";
     printf("%s\n", os.str().c_str());
     return 0;
     Notify(TEX_ICON_SYSTEM, os.str().c_str());
