2148c1ab24f2c84e8b2f6255ccc1f65d126e5f9f
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-04 15:44:31 +0200

    test

--- plugin_Toolbox/source/Utilities.h
+++ plugin_Toolbox/source/Utilities.h
@@ -2,26 +2,6 @@
 
 enum NotifyType
 {
-	NotificationRequest = 0,
-	SystemNotification = 1,
-	SystemNotificationWithUserId = 2,
-	SystemNotificationWithDeviceId = 3,
-	SystemNotificationWithDeviceIdRelatedToUser = 4,
-	SystemNotificationWithText = 5,
-	SystemNotificationWithTextRelatedToUser = 6,
-	SystemNotificationWithErrorCode = 7,
-	SystemNotificationWithAppId = 8,
-	SystemNotificationWithAppName = 9,
-	SystemNotificationWithAppInfo = 9,
-	SystemNotificationWithAppNameRelatedToUser = 10,
-	SystemNotificationWithParams = 11,
-	SendSystemNotificationWithUserName = 12,
-	SystemNotificationWithUserNameInfo = 13,
-	SendAddressingSystemNotification = 14,
-	AddressingSystemNotificationWithDeviceId = 15,
-	AddressingSystemNotificationWithUserName = 16,
-	AddressingSystemNotificationWithUserId = 17,
-
 	UNK_1 = 100,
 	TrcCheckNotificationRequest = 101,
 	NpDebugNotificationRequest = 102,
