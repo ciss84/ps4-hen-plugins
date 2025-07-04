bac76f2540b924644b445db2ad5bb29110993aa1
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-04 15:52:17 +0200

    Update Utilities.h

--- plugin_Toolbox/source/Utilities.h
+++ plugin_Toolbox/source/Utilities.h
@@ -4,14 +4,6 @@
 
 typedef unsigned char vm_prot_t;	/* protection codes */
 
-#define	VM_PROT_NONE		((vm_prot_t) 0x00)
-#define	VM_PROT_READ		((vm_prot_t) 0x01)
-#define	VM_PROT_WRITE		((vm_prot_t) 0x02)
-#define	VM_PROT_EXECUTE		((vm_prot_t) 0x04)
-#define	VM_PROT_COPY		((vm_prot_t) 0x08)	/* copy-on-read */
-
-#define	VM_PROT_ALL		(VM_PROT_READ|VM_PROT_WRITE|VM_PROT_EXECUTE)
-#define VM_PROT_RW		(VM_PROT_READ|VM_PROT_WRITE)
 #define	VM_PROT_DEFAULT		VM_PROT_ALL
 
 #define	MNT_UPDATE	0x0000000000010000ULL /* not real mount, just update */
