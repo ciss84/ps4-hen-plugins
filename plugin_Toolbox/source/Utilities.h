cfc5b42decf15308329de7acea35fdcf774b3972
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-04 15:59:10 +0200

    Update Utilities.h

--- plugin_Toolbox/source/Utilities.h
+++ plugin_Toolbox/source/Utilities.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define SCE_KERNEL_O_WRONLY 0x0001
+#define SCE_KERNEL_O_CREAT 0x0200
+#define SCE_KERNEL_O_RDONLY 0x0000
+
 #define ARRAY_COUNT(arry) sizeof(arry) / sizeof(arry[0])
 
 typedef unsigned char vm_prot_t;	/* protection codes */
